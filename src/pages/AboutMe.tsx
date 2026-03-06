5136d51dcfdf7f1afb77450a5f70be31e37933ac
itzmitto <andre.babirian2004@gmail.com>
2026-03-06 10:52:12 +0100

    card changes

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -6,10 +6,9 @@ function AboutMe() {
             <h1>About Me</h1>
             <p>The things about me.</p>
 
-
             <div className="card">
 
-                <div className="card-content">
+                <div className="card-content1">
                     <h2>Andre</h2>
                     <h1>Developer · Artist · Gamer</h1>
                     <p className="card-text">I'm Andre, a 21-year-old from the Netherlands with a passion for coding, digital art, and gaming. 
@@ -22,7 +21,7 @@ function AboutMe() {
 
             <div className="card">
 
-                <div className="card-content">
+                <div className="card-content2">
                 <h2>Education</h2>
                     <h1>Deltion college Zwolle</h1>
                     <p className="card-text">Software Developer student
@@ -36,7 +35,7 @@ function AboutMe() {
 
             <div className="card">
 
-                <div className="card-content">
+                <div className="card-content3">
                 <h2>Location</h2>
                     <h1>Netherlands, NL</h1>
 
@@ -46,7 +45,7 @@ function AboutMe() {
 
             <div className="card">
 
-                <div className="card-content"> 
+                <div className="card-content4"> 
                 <h2>Age</h2>
                     <h1>21 years old</h1>
                     
@@ -56,7 +55,7 @@ function AboutMe() {
 
              <div className="card">
 
-                <div className="card-content">
+                <div className="card-content5">
                 <h2>Status</h2>
                     <h1>Building stuff</h1>
                     
@@ -66,7 +65,7 @@ function AboutMe() {
 
             <div className="card">
 
-                <div className="card-content">
+                <div className="card-content6">
                 <h2>School</h2>
                     <h1>Deltion College</h1>
 
@@ -76,7 +75,7 @@ function AboutMe() {
 
             <div className="card">
 
-                <div className="card-content">
+                <div className="card-content7">
                 <h2>Interests</h2>
                     <h1>💻 Coding</h1>
                     <h1>🎨 Digital Art</h1>
@@ -91,7 +90,7 @@ function AboutMe() {
             
             <div className="card">
 
-                <div className="card-content">
+                <div className="card-content8">
                 <h2>Currently playing</h2>
                     <h1>Rainbow Six Siege</h1>
                     <h1>Elden Ring</h1>
@@ -109,37 +108,3 @@ function AboutMe() {
 }
 
 export default AboutMe;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

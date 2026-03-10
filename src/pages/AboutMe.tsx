893772f14ddae721d455a21abc78f21de8e2024e
itzmitto <andre.babirian2004@gmail.com>
2026-03-10 14:30:15 +0100

    34ste commit

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,109 +1,68 @@
-import "./AboutMe.css";  
+import "./AboutMe.css";
 
-function AboutMe() {  
+function AboutMe() {
     return (
-        <div>
+        <>
             <h1>About Me</h1>
             <p>The things about me.</p>
-
-            <div className="card">
-
-                <div className="card-content1">
-                    <h2>Andre</h2>
-                    <h1>Developer · Artist · Gamer</h1>
-                    <p className="card-text">I'm Andre, a 21-year-old from the Netherlands with a passion for coding, digital art, and gaming. 
-                    I love building cool things on the web and exploring creative ways to combine tech and art.
-                    When I'm not pushing code, I'm probably grinding a game or sketching something in my sketchbook.
-                    </p>  
-            </div>
-        </div>
+            <div className="wrapper">
+            <div className="gridContainer">
+                <div>
+                    <div className="card about" >
+                        <h3>Andre</h3>
+                        <h1>Developer · Artist · Gamer</h1>
+                        <p>I'm Andre, a 21-year-old from the Netherlands with a passion for coding, digital art, and gaming. I love building cool things on the web and exploring creative ways to combine tech and art. When I'm not pushing code, I'm probably grinding a game or sketching something in my sketchbook.</p>
+                    </div>
+                    <div className="card education">
+                        <h4>Education</h4>
+                        <h3>Deltion College</h3> 
+                        <p>Software Developer student</p>
+                    </div>
+                </div>
                 
+                <div>
+                    <div className="gridContainer">
+                        <div className="card small">
+                            <span>Location</span>
+                        </div>
+                        <div className="card">
+                            <span>Age</span>
+                        </div>
+                        <div className="card small">
+                            <span>Status</span>
+                        </div>
+                        <div className="card">
+                            <span>School</span>
+                        </div>
+                    </div>
 
-            <div className="card">
+                    <div className="card">
+                        <h4>Interests</h4>
+                        <div>
+                            <span>Coding</span>
+                            <span> Digital Art</span>
+                            <span> Gaming</span>
+                            <span> Traditional Art</span>
+                            <span> Soulslike games</span>
+                            <span> Home decor</span>
+                        </div>
+                    </div>
 
-                <div className="card-content2">
-                <h2>Education</h2>
-                    <h1>Deltion college Zwolle</h1>
-                    <p className="card-text">Software Developer student
-                    </p>  
-                    <p className="card-text">Zwolle NL
-                    </p>  
-                    
+                    <div className="card">
+                        <h4>Currently Playing</h4>
+                        <ul>
+                            <li>Rainbow Six Siege</li>
+                            <li>Elden Ring</li>
+                            <li>Dark souls</li>
+                            <li>Enshrouded</li>
+                            <li>Minecraft</li>
+                        </ul>
                     </div>
                 </div>
-
-
-            <div className="card">
-
-                <div className="card-content3">
-                <h2>Location</h2>
-                    <h1>Netherlands, NL</h1>
-
-                    </div>
-                </div>
-
-
-            <div className="card">
-
-                <div className="card-content4"> 
-                <h2>Age</h2>
-                    <h1>21 years old</h1>
-                    
-                    </div>
-                </div>
-
-
-             <div className="card">
-
-                <div className="card-content5">
-                <h2>Status</h2>
-                    <h1>Building stuff</h1>
-                    
-                    </div>
-                </div>
-
-
-            <div className="card">
-
-                <div className="card-content6">
-                <h2>School</h2>
-                    <h1>Deltion College</h1>
-
-                    </div>
-                </div>
-
-
-            <div className="card">
-
-                <div className="card-content7">
-                <h2>Interests</h2>
-                    <h1>💻 Coding</h1>
-                    <h1>🎨 Digital Art</h1>
-                    <h1>🎮 Gaming</h1>
-                    <h1>🖌️ Traditional Art</h1>
-                    <h1>⚔️ Soulslike games</h1>
-                    <h1>🏡 Home decor</h1>
-                    
-                    </div>
-                </div>
-
-            
-            <div className="card">
-
-                <div className="card-content8">
-                <h2>Currently playing</h2>
-                    <h1>Rainbow Six Siege</h1>
-                    <h1>Elden Ring</h1>
-                    <h1>Dark Souls</h1>
-                    <h1>Enshrouded</h1>
-                    <h1>Minecraft</h1>
-                    </div>
-                </div>
-
-
-
-
             </div>
+            </div>
+            
+        </>
     )
 }
 

7b61a91ac8ad24106c9d19002cd53544c59db249
itzmitto <andre.babirian2004@gmail.com>
2026-04-07 13:14:50 +0200

    245ste commit

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -9,7 +9,7 @@ function Projects() {
                     <div className="titleBlock">
                         <h1>── My work</h1>
                         <h2>Things I've built.</h2>
-                        <p>A mix of web projects, creative experiments, and things I'm
+                        <p>A mix of web projects, creative experiments, and things I'm<br/>
                         currently building. Always working on something new.</p>
                     </div>
 

55d45893e1f55fbe5eddfa3fa89ecb67ad852d96
itzmitto <andre.babirian2004@gmail.com>
2026-04-08 11:09:11 +0200

    256ste commit

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -13,8 +13,44 @@ function Contact() {
                             currently building. Always working on something new.
                         </p>
                     </div>
+
+
+                    <div className="cardsContainer">
+                        <div className="card main">
+                            <span className="cardNumber">01</span>
+                            <h1>Portfolio website</h1>
+                            <h2>Webapp</h2>
+                            <h3>This portfolio you're looking at right now built with React and TypeScript to showcase my work and skills.</h3>
+                            <div className="pillsWrapper">
+                                <span className="pill">React</span>
+                                <span className="pill">Typescript</span>
+                                <span className="pill">Css</span>
+                            </div>
+                        </div>
+
+
+
+                        <div className="card second">
+                            <span className="cardNumber">02</span>
+                            <h1>Coming soon</h1>
+                            <h2>Project</h2>
+                            <h3>Building something new. Check back soon for more projects as I keep creating and coding cool things.</h3>
+                            <div className="pillsWrapper">
+                                <span className="pill">react</span>
+                                <span className="pill">Laravel</span>
+                            </div>
+                        </div>
+
+
+
+
+
+
+
+
                 </div>
             </div>
+        </div>
         </div>
 
 
